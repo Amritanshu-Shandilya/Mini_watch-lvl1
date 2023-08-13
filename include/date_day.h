983eb2a64cab1af4cbe34e882d41c41a31a49e8b
Amritanshu Shandilya <starkshivam1611@gmail.com>
2023-08-13 16:58:24 +0530

    Added code for displaying hours and minutes

--- include/date_day.h
+++ include/date_day.h
@@ -28,3 +28,25 @@ void displayDay(int w_day){
         display.println("Sunday");
     }
 }
+
+
+void display_hr(int h,int m){
+    int h1,m1,h2,m2;
+    if (h<9){
+        h1 = 0;
+        h2 = h;
+    }
+    else {
+        h1 = h / 10;
+        h2 = h % 10;
+    }
+    if (m<9){
+        m1 = 0;
+        m2 = m;
+    }
+    else{
+        m1 = m / 10;
+        m2 = m % 10;
+    }
+    drawWatchFace(h1, h2, m1, m2);
+}
